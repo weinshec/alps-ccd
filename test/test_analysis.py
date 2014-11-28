cf0c078388137ebd67e30f371775104a36ef3d7e
Eike von Seggern <eikevons@yahoo.de>
2014-11-28 11:50:54 +0100

    make normlize.py Py3 compatible+add test for FrameNormalizer

--- test/test_analysis.py
+++ test/test_analysis.py
@@ -3,7 +3,7 @@ from itertools import combinations
 import numpy as np
 
 from ccd.analysis.spotfitting import fwhm
-from ccd.analysis.normalize import rowwise_offset
+from ccd.analysis.normalize import rowwise_offset, FrameNormalizer
 
 class TestHelperFunctions(unittest.TestCase):
     def test_fwhm(self):
@@ -15,19 +15,84 @@ class TestHelperFunctions(unittest.TestCase):
         self.assertEqual(i1, 8)
 
 
-class TestNormalizer(unittest.TestCase):
+class TestNormalize(unittest.TestCase):
     def test_rowwise_offset(self):
-        data = np.ones((100, 100)) * np.random.rand(100)[:,np.newaxis]
+        """Test calling rowwise_offset.
 
-        c1 = data - rowwise_offset((90, None), data)
-        c2 = data - rowwise_offset(slice(90, None), data)
-        c3 = data - rowwise_offset(slice(90, None))(data)
-        c4 = data - rowwise_offset((90, None))(data)
+        Covered::
+            * Calling directly (via __new__) or using __call__ .
+            * Specifying columns as tuple or slice.
+        """
+        ncol = 100
+        nrow = 200
+        data = np.ones((nrow, ncol)) * np.random.rand(nrow)[:,np.newaxis]
 
-        self.assertTrue((c1 < 1e-14).all())
+        corrected = (
+            # Applied directly in __new__ ...
+            # ... with column-index tuple
+            data - rowwise_offset((90, None), data),
+            # ... with column-slice
+            data - rowwise_offset(slice(90, None), data),
+            # ... with column index-array
+            data - rowwise_offset(range(90, 100), data),
 
-        for a, b in combinations((c1, c2, c3, c4), 2):
+            # Applied using __call__ ...
+            # ... with column-index tuple
+            data - rowwise_offset((90, None))(data),
+            # ... with column-slice
+            data - rowwise_offset(slice(90, None))(data),
+            # ... with column index-array
+            data - rowwise_offset(range(90, 100))(data),
+        )
+
+        self.assertTrue((corrected[0] < 1e-14).all())
+
+        for a, b in combinations(corrected, 2):
             self.assertTrue((a == b).all())
+
+    def test_normalizer(self):
+        """Test FrameNormalizer
+
+        Covered::
+            * Correcting row-wise offset.
+            * Extracting data region.
+        """
+        ncol = 100
+        nrow = 200
+        # data window
+        data_x1, data_x2 = 10, 80
+        data_y1, data_y2 = 10, 180
+        # overscan columns
+        overscan_cols = slice(90, None)
+        # overscan offset
+        offset = 3.3
+        # maximum index
+        x_max = 5
+        y_max = 8
+
+        normalizer = FrameNormalizer((nrow, ncol),
+                                     (data_y1, data_y2),
+                                     (data_x1, data_x2),
+                                     rowwise_offset(overscan_cols))
+
+        # Prepare dummy data with per-row offset
+        raw_data = np.ones(ncol) * np.arange(nrow)[:, np.newaxis]
+        # ... add global overscan column offset
+        raw_data[:, overscan_cols] += offset
+        # ... add single maximum entry
+        raw_data[y_max + data_y1, x_max + data_x1] += 1e4
+
+        offset_corrected = normalizer.correct_offset(raw_data)
+        data_region = normalizer.data_region(offset_corrected)
+
+        self.assertEqual((y_max, x_max),
+                         np.unravel_index(data_region.argmax(),
+                                          data_region.shape))
+        rows = list(range(data_region.shape[0]))
+        rows.remove(y_max)
+        for y1, y2 in combinations(rows, 2):
+            self.assertTrue((data_region[y1] - data_region[y2] < 1e-14).all())
+
 
 
 if __name__ == "__main__":
